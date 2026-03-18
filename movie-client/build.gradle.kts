2a77bc055d1c17f15630ea6493724d4f5ddb6bde
vpl <vpl@vpl.me>
2026-03-18 22:35:45 +0100

    Add micrometer dependencies

--- movie-client/build.gradle.kts
+++ movie-client/build.gradle.kts
@@ -15,10 +15,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-micrometer-metrics")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     // OTLP metrics export (pushes to Prometheus via OTLP)
-    implementation("io.micrometer:micrometer-registry-otlp")
+    runtimeOnly("io.micrometer:micrometer-registry-otlp")
 }
 
 kotlin {
--- movie-service/build.gradle.kts
+++ movie-service/build.gradle.kts
@@ -14,10 +14,11 @@ java {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-micrometer-metrics")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     // OTLP metrics export (pushes to Prometheus via OTLP)
-    implementation("io.micrometer:micrometer-registry-otlp")
+    runtimeOnly("io.micrometer:micrometer-registry-otlp")
 }
 
 kotlin {
